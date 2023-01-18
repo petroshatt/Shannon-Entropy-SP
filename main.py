14ed1e0a50197d24812f0677610e245f90b34ea7
Petros Chatzitoulousis <petroshatt@gmail.com>
2023-01-18 18:57:35 +0200

    Fixed Structure

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import scipy
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def ShEn(EEG_data):
+    return 0
+
+def EnPlot(enZ, enO, enN, enF, enS):
+    return 0
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    mat = scipy.io.loadmat('BonnEEGdata.mat')
+    Z_EEG_data = mat['Z_EEG_data']
+    O_EEG_data = mat['O_EEG_data']
+    N_EEG_data = mat['N_EEG_data']
+    F_EEG_data = mat['F_EEG_data']
+    S_EEG_data = mat['S_EEG_data']
+
+    enZ = ShEn(Z_EEG_data)
+    enO = ShEn(O_EEG_data)
+    enN = ShEn(N_EEG_data)
+    enF = ShEn(F_EEG_data)
+    enS = ShEn(S_EEG_data)
+
+    EnPlot(enZ, enO, enN, enF, enS)
+
+
