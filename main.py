62ace8e4c3830d677a3d075fb124d7bf6d799f3a
Petros Chatzitoulousis <petroshatt@gmail.com>
2023-01-19 18:01:38 +0200

    Added comments

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import math
 
 import scipy
 import numpy as np
-import pandas as pd
 from collections import defaultdict
-from sklearn.preprocessing import KBinsDiscretizer
 
 
 def discretize(EEG_data):
+    """
+    Discretization function finds the bounds of each class, and converts each
+    signal value to a class - NEEDS TO BE IMPORVED
+    Here the discretization classes are 10 and each value is converted to 0-9
+    :param EEG_data: The ndarray to be discretized
+    :return: The discretized ndarray
+    """
 
     discr_classes = 10
-
     bounds = []
 
     epsilon = int((EEG_data.max() - EEG_data.min()) / discr_classes)
@@ -20,6 +24,10 @@ def discretize(EEG_data):
         boundToAdd += epsilon
         bounds.append(boundToAdd)
     bounds.append(EEG_data.max())
+
+    print("Min: ", EEG_data.min())
+    print("Max: ", EEG_data.max())
+    print(bounds)
 
     for i in range(len(EEG_data)):
         for j in range(len(EEG_data[0])):
@@ -44,10 +52,19 @@ def discretize(EEG_data):
             elif bounds[9] <= EEG_data[i][j] <= bounds[10]:
                 EEG_data[i][j] = 9
 
+    unique, counts = np.unique(EEG_data, return_counts=True)
+    print(np.asarray((unique, counts)).T)
+
     return EEG_data
 
 
 def ShEn(EEG_data):
+    """
+    Shannon Entropy calculation, counting patterns of a specific length using a dict,
+    calculating each pattern's probability and using it for the Shannon Entropy formula
+    :param EEG_data: The dataset's subset
+    :return: Subset's calculated Shannon Entropy
+    """
 
     EEG_data = discretize(EEG_data)
 
@@ -68,6 +85,7 @@ def ShEn(EEG_data):
 
 
 def EnPlot(enZ, enO, enN, enF, enS):
+
     return 0
 
 
@@ -81,16 +99,15 @@ if __name__ == '__main__':
     S_EEG_data = mat['S_EEG_data']
 
     enZ = ShEn(Z_EEG_data)
-    print("Z_EEG_data entropy: %.3f" % enZ)
+    print("Z_EEG_data entropy: %.3f\n" % enZ)
     enO = ShEn(O_EEG_data)
-    print("O_EEG_data entropy: %.3f" % enO)
+    print("O_EEG_data entropy: %.3f\n" % enO)
     enN = ShEn(N_EEG_data)
-    print("N_EEG_data entropy: %.3f" % enN)
+    print("N_EEG_data entropy: %.3f\n" % enN)
     enF = ShEn(F_EEG_data)
-    print("F_EEG_data entropy: %.3f" % enF)
+    print("F_EEG_data entropy: %.3f\n" % enF)
     enS = ShEn(S_EEG_data)
-    print("S_EEG_data entropy: %.3f" % enS)
-
+    print("S_EEG_data entropy: %.3f\n" % enS)
     # EnPlot(enZ, enO, enN, enF, enS)
 
 
